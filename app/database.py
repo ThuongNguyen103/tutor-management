f3005e9f618fd89aa086b585c50eaea304264d29
thuongthuongthuong <huuthuongaa1@gmail.com>
2026-06-09 23:47:37 +0700

    fix

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 # 1. Lấy DATABASE_URL từ biến môi trường của hệ thống (Render sẽ cấp biến này)
 # Nếu không tìm thấy (khi chạy ở máy cá nhân), sẽ tự động dùng chuỗi localhost hiện tại của bạn.
 DATABASE_URL = os.getenv(
-    "postgresql://tutor:aFGlg9tRzFqxQeWf5zjeGMqnLtqlwnYU@dpg-d8k44sf7f7vs73btgg90-a.singapore-postgres.render.com/tutor_management_6z9i",
+    "DATABASE_URL",
     "postgresql+psycopg2://tutor:tutor123@localhost:5432/tutor_management"
 )
 
